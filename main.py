d43785ceba1dbd177c6bb97c86ca4149a55298f5
maria.mingazova <maria.mingazova@telekom.com>
2021-10-25 12:41:45 +0300

    600_000 DAG 7z

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import json
+
 
 def read_edges(edges_line):
     args = edges_line.split("	")
+    if len(args) == 1:
+        args = edges_line.split(" ")
 
     if len(args) == 2:
         return [args[0], args[1]]
@@ -13,7 +17,7 @@ def read_edges(edges_line):
 
 if __name__ == '__main__':
 
-    with open('./originalData/DAG_685_000_nodes.txt', 'r') as orgn, open('./dotData/DAG_685_000_nodes.dot', 'w+') as dstn:
+    with open('./originalData/Task_10_000_nodes_27_000_edges.txt', 'r') as orgn, open('./dotArchives/Task_graph_10_000_nodes.dot', 'w+') as dstn:
         dstn.write('digraph {\n')
 
         for line in orgn.readlines():
@@ -23,5 +27,20 @@ if __name__ == '__main__':
             dstn.write(dot_edges)
 
         dstn.write('}\n')
+
+    # with open('./originalData/Task_g_5_000_n_18_000_e.txt', 'r') as orgn, open('./dotArchives/Task_graph_5_000_nodes.json', 'w+') as dstn:
+    #     dstn.write('[\n')
+    #
+    #     for line in orgn.readlines():
+    #         origin_edges = read_edges(line)
+    #
+    #         data_set = {"source": origin_edges[0], "target": origin_edges[1]}
+    #
+    #         json_dump = json.dumps(data_set)
+    #
+    #         dstn.write(json_dump)
+    #         dstn.write(',\n')
+    #
+    #     dstn.write(']\n')
 
 
